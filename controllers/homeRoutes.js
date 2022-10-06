2e258e23ad5a6b32c15200fc0d5a803f110159ed
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-06 13:55:43 +0100

    added findall posts to homeroutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'text', 'date_created', 'user_id', 'post_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+          }
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        posts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
