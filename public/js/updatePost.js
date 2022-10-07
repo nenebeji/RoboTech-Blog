aebacd74861210df17bf3d6d034411c5320f9644
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-07 11:23:29 +0100

    added update function

---
+++ public/js/updatePost.js
@@ -0,0 +1,29 @@
+
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const post_content = document.querySelector('.post-content').value.trim();
+
+    if(title && post_content){
+        const response = await fetch(`/api/posts/${post_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title,
+                post_content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        if (response.ok) {
+            document.location.replace('/user-dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+
+}
+
+document.querySelector('.edit-form').addEventListener('submit', editFormHandler);
