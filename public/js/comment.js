c42966a5f375c027b2950ddc4c601d6a97b07493
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-10-07 11:33:17 +0100

    added comment form function

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-text"]').value.trim();
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
